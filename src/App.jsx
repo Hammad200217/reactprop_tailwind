6d44ff5480dd4c3bb6f138d145b1712e63fa7e6d
Hammad Ali <hammadali78056@gmail.com>
2025-06-23 15:30:06 +0500

    new updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Card from './components/card'
 import './App.css'
 
 function App() {
@@ -8,7 +9,9 @@ function App() {
 
   return (
     <>
-     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+     {/* <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1> */}
+     <Card  Name="Techo Soft" btntext="Read me"/>
+       <Card Name="Aplha Soft" btntext="Click me"/>
     </>
   )
 }
